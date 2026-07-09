500179ab286f82120952ebd081852e4656fe7094
hasalpcode <hasalpcode784@gmail.com>
2026-07-09 18:54:12 -0400

    Ajout des en-têtes CORS

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -195,3 +195,11 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+
+    def test_cors_security(self):
+        """Il devrait renvoyer un en-tête CORS"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Vérifiez la présence de l'en-tête CORS
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
